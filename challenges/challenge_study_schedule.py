432ee466428f762bbb4c0d6f8b3c8e33bd39b537
polidex <lp.igor@gmail.com>
2023-03-28 17:05:16 -0300

    feat: reqt 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    
+    count = 0
+
+    for logIn, logOut in permanence_period:
+        if type(logIn) != int or type(logOut) != int:
+            return None
+        if logIn <= target_time <= logOut:
+            count += 1
+
+    return count
+
