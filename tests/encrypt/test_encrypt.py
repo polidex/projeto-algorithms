ff51503f8d9ca278971c603fcc0fb85c0918e9d2
polidex <lp.igor@gmail.com>
2023-03-28 18:22:17 -0300

    test: reqt 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message('polido', 'i')
+
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(765, 4)
+
+    assert encrypt_message('polido', 3) == 'lop_odi'
+
+    assert encrypt_message('polido', 4) == 'od_ilop'
+
+    assert encrypt_message('polido', 9) == 'odilop'
