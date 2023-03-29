9a7705a264cac53168a72f923bdf797a006c0ceb
polidex <lp.igor@gmail.com>
2023-03-29 16:20:03 -0300

    feat: reqts 3 e 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,22 @@
+def sort_string(string):
+    if len(string) <= 1:
+        return string
+
+    firstLetter = string[0]
+    first_index = [letter for letter in string[1:] if letter < firstLetter]
+    last_index = [letter for letter in string[1:] if letter >= firstLetter]
+
+    return sort_string(first_index) + [firstLetter] + sort_string(last_index)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (first_string == '') and (second_string == ''):
+        return ('', '', False)
+
+    first_word = first_string .lower()
+    second_word = second_string.lower()
+
+    return (''.join(sort_string(list(first_word))),
+            ''.join(sort_string(list(second_word))),
+            ''.join(sort_string(list(first_word)))
+            == ''.join(sort_string(list(second_word))))
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    if low_index == len(word) or high_index < 0:
+        return True
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    high_index -= 1
+    low_index += 1
+
+    return is_palindrome_recursive(word, low_index, high_index) is True
